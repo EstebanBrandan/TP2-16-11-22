c97e380377675edd3de6237f7a398aa21974e48b
EstebanBrandan <100619224+EstebanBrandan@users.noreply.github.com>
2022-11-16 21:55:15 -0300

    Add files via upload

---
+++ Juego De La Vida 3D/src/Celula.cpp
@@ -0,0 +1,58 @@
+/*
+ * Celula.cpp
+ *
+ *  Created on: 11 oct. 2022
+ *      Author: luis
+ */
+
+#include "Celula.h"
+
+Celula::Celula() {
+	this->gen.genAlfa=0;
+	this->gen.genBeta=0;
+	this->gen.genGamma=0;
+	this->estado=muerto;
+}
+
+void Celula::setEstado(Estado estado){
+	this->estado=estado;
+}
+
+void Celula::setCelulaViva(){
+	unsigned int genAleatorio1=rand();
+	unsigned int genAleatorio2=rand();
+	unsigned int genAleatorio3=rand();
+	this->gen.genAlfa=(genAleatorio1 % 256);
+	this->gen.genBeta=(genAleatorio2 % 256);
+	this->gen.genGamma=(genAleatorio3 % 256);
+	this->estado=vivo;
+}
+
+void Celula::setCelulaMuerta(){
+	this->gen.genAlfa=0;
+	this->gen.genBeta=0;
+	this->gen.genGamma=0;
+	this->estado=muerto;
+}
+
+void Celula::setCelulaVecina(Celula* vecina, int i){
+	this->vecinos[i]=vecina;
+}
+
+Estado Celula::mostrarCelulasVecinas(int i){
+	Celula* vecino=this->vecinos[i];
+	return vecino->getEstado();
+}
+
+Estado Celula::getEstado(){
+	return this->estado;
+}
+
+Gen Celula::getGenes(){
+	return this->gen;
+}
+
+Celula::~Celula() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Juego De La Vida 3D/src/main.cpp
@@ -0,0 +1,52 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 11 oct. 2022
+ *      Author: luis
+ */
+#include <iostream>
+#include "Tablero.h"
+#include "ReadData.h"
+
+int main(){
+
+	std::string Datos1= "src/DataMap.txt";
+
+		 // prints !!!Hello World!!!
+		vector<string> datos;
+		vector<string> config;
+		tomarDatosDeArchivoTXT(Datos1,datos, config);
+		/*
+		for(int i=0;i<=config.size();i++){
+			std::cout<<config[i]<<std::endl;
+		}*/
+
+		std::cout<<"test"<<std::endl;
+
+		/*for(int i=0;i<=datos.size();i++){
+			std::cout<<datos[i]<<std::endl;
+		}*/
+
+
+
+	Tablero * tablero=new Tablero(stoi(config[0]), stoi(config[1]), stoi(config[2]));
+	tablero->cargarDatosVector(datos);
+
+
+	tablero->mostrarTablero();
+	tablero->cargarVecinos();
+	/*int j=2;
+	Celula* aux=tablero->getCasillero(j, j, j)->getCelula();
+	std::cout<<"Celulas vecinas vivas en la posicion ("<<j<<","<<j<<","<<j<<")."<<std::endl;
+	for (int i=0; i<26; i++){
+		if (aux->mostrarCelulasVecinas(i)==vivo){
+			std::cout<<"vivo-";
+		}else{
+			std::cout<<"muerto-";
+		}
+	}
+	std::cout<<std::endl;*/
+	return 0;
+}
+
+
